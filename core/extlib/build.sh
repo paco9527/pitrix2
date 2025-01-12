cec1e0dc12b4748bf16bb4c02cfb8a9d739aa726
cloudzone <italibur@qq.com>
2025-01-12 13:48:41 +0800

    upgrade build script

--- core/extlib/build.sh
+++ core/extlib/build.sh
@@ -1,8 +1,14 @@
-CUR_DIR=$(pwd)
+#!/bin/bash
+CUR_DIR=$1
 BUILD_TYPE=Release
-CROSS_C_COMPILER=aarch64-linux-gnu-gcc
-CROSS_CXX_COMPILER=aarch64-linux-gnu-g++
+CROSS_C_COMPILER=$2
+CROSS_CXX_COMPILER=$3
 CMAKE_COMPILE_FLAGS="-DCMAKE_C_COMPILER=$CROSS_C_COMPILER -DCMAKE_CXX_COMPILER=$CROSS_CXX_COMPILER -DCMAKE_INSTALL_PREFIX=$CUR_DIR/output -DCMAKE_BUILD_TYPE=$BUILD_TYPE"
+
+pushd $CUR_DIR
+mkdir -p output
+mkdir -p output/include
+mkdir -p output/lib
 
 # lua
 pushd lua
@@ -13,6 +19,7 @@ popd
 
 # lvgl
 pushd lvgl
+cp ../patch/lv_conf.h .
 mkdir build
 cd build
 cmake .. $CMAKE_COMPILE_FLAGS
@@ -23,7 +30,9 @@ popd
 # luavgl
 cp CMakeLists.extlib.tostatic luavgl/CMakeLists.txt
 pushd luavgl
-mkdir build
+# v0.1.0编译时少包含了头文件；暂时不打算尝试luavgl上最新的提交了，就这么对付一下
+git apply ../patch/luavgl-v0.1.0.patch
+mkdir -p build
 cd build
 cmake .. -DSRC_PATH=$CUR_DIR/luavgl/src -DEXTERN_INCLUDE_PATH="$CUR_DIR/output/include/lvgl;$CUR_DIR/luavgl/src;$CUR_DIR/lua" $CMAKE_COMPILE_FLAGS -DLIBRARY_NAME="luavgl"
 make -j
@@ -49,10 +58,16 @@ popd
 # lua-cjson
 cp CMakeLists.extlib.tostatic lua-cjson/CMakeLists.txt
 pushd lua-cjson
+# 用一个比较傻的办法让它使用fpconv.c...
+mv dtoa.c dtoa.c.disable
 mkdir build
 cd build
 cmake .. -DSRC_PATH=$CUR_DIR/lua-cjson -DEXTERN_INCLUDE_PATH="$CUR_DIR/lua" $CMAKE_COMPILE_FLAGS -DLIBRARY_NAME="lua-cjson"
 make -j
 cp liblua-cjson.a $CUR_DIR/output/lib
 cp ../*.h $CUR_DIR/output/include
+cd ..
+mv dtoa.c.disable dtoa.c
+popd
+
 popd
